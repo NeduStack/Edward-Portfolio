482583233f24aa0777aa5877cb171c2d9b8c1b02
Ugwoke Edward Chinedu <nedu.stack@gmail.com>
2025-01-30 13:12:32 +0000

    Create Hero.jsx
    Updated Name

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,70 @@
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/hero-img.gif';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/EDWARD-CV2.pdf'
+import { useTheme } from '../../common/ThemeContext';
+
+function Hero() {
+    const { theme, toggleTheme } = useTheme();
+
+    const themeIcon = theme === 'light' ? sun : moon;
+    const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+    const githubIcon = theme === 'light' ? githubLight :githubDark;
+    const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+  return (
+    <section id="hero" className={styles.container}>
+        <div className={styles.coloModeContainer} > 
+            <img className={styles.hero} 
+                src={heroImg} 
+                alt="Profile picture of Edward" 
+            /> 
+            <img className={styles.colorMode} 
+                src={themeIcon}
+                alt="Color mode Icon"
+                onClick={toggleTheme}
+            />
+        </div>
+        <div className={styles.Info}>
+            <h1>
+                Ugwoke Edward Chinedu
+            </h1>
+            <h2> Software Engineer </h2>
+            <span>
+                <a href="https://twitter.com" target="_blank">
+                    <img src={twitterIcon} alt="Twitter icon" />
+                </a>
+            
+                <a href="https://github.com/NeduStack" target="_blank">
+                    <img src={githubIcon} alt="Github icon" />
+                </a>
+        
+                <a href="https://www.linkedin.com/in/edward-ugwoke-32526b145?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blank">
+                    <img src={linkedinIcon} alt="Linkedin icon" />
+                </a>
+            </span>
+            <p>
+                With zeal for developing modern and futuristic React and Django Web app and python scripts to accomplish complex tasks for individual and commercial businesses.
+            </p>
+
+             <br />
+             <br />
+
+            <a href={CV} download>
+                <button className="hover">
+                    Resume
+                </button>
+            </a>
+
+        </div>
+    </section>
+  )
+}
+
+export default Hero;
